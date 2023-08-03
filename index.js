651967b9543563ec98de2d2139df60bb3389895a
+ <+>
2023-08-03 13:25:29 -0600

    finished

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    var upperCaseName = customerName.toUpperCase();
+    return upperCaseName;
+}
+
+var bestCustomer = 'maybe  bob';
+
+function setBestCustomer(){
+    var bestCustomer = 'maybe bob';
+}
+setBestCustomer();
+
+
+var bestCustomer = 'not bob';
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+overwriteBestCustomer();
+
+const leastFavoriteCustomer = 'John';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Alice';
+}
